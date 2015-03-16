4da0e5fe38539e5745e1b36cdb9f767c443b14d4
Omer Akram <om26er@gmail.com>
2015-03-16 17:14:04 +0500

    Some code cleanup, introduce a new UiUpdateHelpers class, fix crashes

---
+++ app/src/main/java/com/byteshaft/namaztime/UiUpdateHelpers.java
@@ -0,0 +1,46 @@
+package com.byteshaft.namaztime;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+public class UiUpdateHelpers {
+
+    private Activity mActivity;
+
+    public UiUpdateHelpers(Activity context) {
+        mActivity = context;
+    }
+
+    public void setDate(String date) {
+        TextView dateLabel = (TextView) mActivity.findViewById(R.id.dateLabel);
+        setupLabels(dateLabel, date);
+    }
+
+    public void setNamazNames(String namaz) {
+        TextView namazLabel = (TextView) mActivity.findViewById(R.id.namazLabel);
+        setupLabels(namazLabel, namaz);
+    }
+
+    public void setNamazTimesLabel(String namazTimes) {
+        TextView namazTimesLabel = (TextView) mActivity.findViewById(R.id.namazTimesLabel);
+        setupLabels(namazTimesLabel, namazTimes);
+    }
+
+    private void setupLabels(TextView textView, String text) {
+        int TEXT_SIZE = 20;
+        textView.setTypeface(getTypeface());
+        textView.setTextSize(TEXT_SIZE);
+        textView.setTextColor(getCustomTextColorCode());
+        textView.setText(text);
+    }
+
+    private Typeface getTypeface() {
+        return Typeface.create("sans-serif", Typeface.BOLD);
+    }
+
+    private int getCustomTextColorCode() {
+        return Color.parseColor("#FFFFFF");
+    }
+}
