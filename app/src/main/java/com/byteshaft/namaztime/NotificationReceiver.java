6a982367fa4d97bf917431ddaa7afd3d3d3f1aef
bilal shahid <s9iper1@gmail.com>
2015-04-29 12:15:13 +0500

    add LGPL license

--- app/src/main/java/com/byteshaft/namaztime/NotificationReceiver.java
+++ app/src/main/java/com/byteshaft/namaztime/NotificationReceiver.java
@@ -1,5 +1,15 @@
-package com.byteshaft.namaztime;
+/*
+ *
+ *  * (C) Copyright 2015 byteShaft Inc.
+ *  *
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the GNU Lesser General Public License
+ *  * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ *  * http://www.gnu.org/licenses/lgpl-2.1.html
+ *  
+ */
 
+package com.byteshaft.namaztime;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
